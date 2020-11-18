1eaa7140f5724353930739b4a00fc15a1b26eecf
VarunDelft <nair.varun1992@gmail.com>
2020-11-18 13:18:58 +0100

    add day1 solutions

---
+++ Day1/src/com/dayone/partone/PartOne.java
@@ -0,0 +1,49 @@
+package com.dayone.partone;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static java.lang.Math.floor;
+
+public class PartOne {
+
+    public static void main(String[] args) {
+
+        try {
+
+            File file = new File("input.txt");
+            Scanner inputScanner = new Scanner(file);
+
+            int totalFuelRequirement = calculateTotalFuelRequirement(inputScanner);
+
+            System.out.println("The total fuel requirement is " + totalFuelRequirement);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static int calculateTotalFuelRequirement(Scanner inputScanner){
+        int mass;
+        int sumFuelRequirements = 0;
+
+        while (inputScanner.hasNextLine()){
+
+            String line = inputScanner.nextLine();
+            mass = Integer.parseInt(line);
+            sumFuelRequirements += requiredFuelPerModule(mass);
+
+        }
+
+        inputScanner.close();
+
+        return sumFuelRequirements;
+    }
+
+    private static int requiredFuelPerModule(int mass){
+        return (int) (floor(mass/3) - 2);
+    }
+
+}
---
+++ Day1/src/com/dayone/parttwo/PartTwo.java
@@ -0,0 +1,61 @@
+package com.dayone.parttwo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static java.lang.Math.floor;
+import static java.lang.Math.max;
+
+public class PartTwo {
+
+    public static void main(String[] args) {
+
+        try {
+
+            File file = new File("input.txt");
+            Scanner inputScanner = new Scanner(file);
+
+            int totalFuelRequirement = calculateTotalFuelRequirement(inputScanner);
+
+            System.out.println("The total fuel requirement is " + totalFuelRequirement);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static int calculateTotalFuelRequirement(Scanner inputScanner){
+        int mass;
+        int sumFuelRequirements = 0;
+
+        while (inputScanner.hasNextLine()){
+
+            String line = inputScanner.nextLine();
+            mass = Integer.parseInt(line);
+            sumFuelRequirements += requiredFuelPerModule(mass);
+
+        }
+
+        inputScanner.close();
+
+        return sumFuelRequirements;
+    }
+
+    private static int requiredFuelPerModule(int mass){
+
+        int fuel = (int) (floor(mass/3) - 2);
+        int newMass = fuel;
+
+        while (newMass > 0){
+            newMass = (int) (floor(newMass/3) - 2);
+            newMass = max(newMass, 0);
+            fuel += newMass;
+        }
+
+        return fuel;
+
+    }
+
+}
